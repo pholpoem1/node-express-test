571fdfb8d1d4d80fa5c96cf8642279147544e6dc
nueng.jp <jintana.ph@bop.co.th>
2023-06-03 21:58:04 +0700

    web page

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const chalk = require("chalk");
 const debug = require("debug")("app");
 const morgan = require("morgan");
+const path = require("path");
+
 const app = express();
 const port = 4000;
 
 app.use(morgan("combind"));
+app.use(express.static(path.join(__dirname, "/src/")));
 
 app.get("/", (req, res) => {
   res.send("Hello Jintana");
